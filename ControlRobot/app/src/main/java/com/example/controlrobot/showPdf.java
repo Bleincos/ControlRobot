fd1e0d81e6b149c0dc44b73240e5a5802d84cb28
Bleinc <olivier.bleinc@groupe-esigelec.org>
2020-04-13 22:50:34 +0200

    Pdf read
    Ajout de la lecture de pdf

---
+++ ControlRobot/app/src/main/java/com/example/controlrobot/showPdf.java
@@ -0,0 +1,34 @@
+package com.example.controlrobot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+public class showPdf extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.show_pdf);
+
+        Button back;
+        PDFView apdfView;
+        apdfView = findViewById(R.id.pdfView);
+        apdfView.fromAsset("example.pdf").load();
+
+        back = findViewById(R.id.buttonBack);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(showPdf.this, About.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
