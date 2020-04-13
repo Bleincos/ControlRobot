9a522da37125830867ceeee4607716f4469791f4
Bleinc <olivier.bleinc@groupe-esigelec.org>
2020-04-13 19:25:00 +0200

    First test
    First run of app

--- ControlRobot/app/src/main/java/com/example/controlrobot/About.java
+++ ControlRobot/app/src/main/java/com/example/controlrobot/About.java
@@ -3,6 +3,9 @@ package com.example.controlrobot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class About extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        TextView textView;
+
+        textView = findViewById(R.id.TextViewAbout);
+        textView.setText("App developped by : \n\r Marwane Ould-Ahmed \n\r And \n\r Olivier Bleinc \n\r This app allows the user to control a robot directly from his phone to the robot via bluetooth. Click How to use to show instructions");
     }
 }
--- ControlRobot/app/src/main/java/com/example/controlrobot/MainActivity.java
+++ ControlRobot/app/src/main/java/com/example/controlrobot/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.controlrobot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button about;
+
+        about = findViewById(R.id.buttonAbout);
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, About.class);
+                startActivity(intent);
+                finish();
+            }
+            });
     }
 }
