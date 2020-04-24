815c5f1acaa7a86f932585f5e451d7e04e87dff2
Bleinc <olivier.bleinc@groupe-esigelec.org>
2020-04-24 11:14:38 +0200

    main act

--- Tryhome/app/src/main/java/com/example/tryhome/MainActivity.java
+++ Tryhome/app/src/main/java/com/example/tryhome/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
         showInstruction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Steering.class);
+                Intent intent = new Intent(MainActivity.this, showPdf.class);
                 startActivity(intent);
                 finish();
             }
