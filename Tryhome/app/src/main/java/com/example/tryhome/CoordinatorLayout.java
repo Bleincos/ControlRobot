869a4d5f7ee7f13d6936dd492562ed29b42f9443
MaGit98 <marwane.ouldahmed1@gmail.com>
2020-04-24 10:30:28 +0200

    app bar

---
+++ Tryhome/app/src/main/java/com/example/tryhome/CoordinatorLayout.java
@@ -0,0 +1,58 @@
+package com.example.tryhome;
+
+import androidx.annotation.NonNull;
+
+import androidx.core.view.NestedScrollingParent2;
+import androidx.core.view.NestedScrollingParent3;
+
+import java.lang.Object;
+
+import android.content.Context;
+import android.view.View;
+import 	android.view.ViewGroup;
+
+
+
+public class CoordinatorLayout extends ViewGroup implements NestedScrollingParent2, NestedScrollingParent3
+
+
+{
+    public CoordinatorLayout(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+
+    @Override
+    public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type, @NonNull int[] consumed) {
+
+    }
+
+    @Override
+    public boolean onStartNestedScroll(@NonNull View child, @NonNull View target, int axes, int type) {
+        return false;
+    }
+
+    @Override
+    public void onNestedScrollAccepted(@NonNull View child, @NonNull View target, int axes, int type) {
+
+    }
+
+    @Override
+    public void onStopNestedScroll(@NonNull View target, int type) {
+
+    }
+
+    @Override
+    public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
+
+    }
+
+    @Override
+    public void onNestedPreScroll(@NonNull View target, int dx, int dy, @NonNull int[] consumed, int type) {
+
+    }
+}
--- Tryhome/app/src/main/java/com/example/tryhome/Steering.java
+++ Tryhome/app/src/main/java/com/example/tryhome/Steering.java
@@ -2,7 +2,9 @@ package com.example.tryhome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -26,6 +28,8 @@ public class Steering extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_steering);
+
+
         speed = findViewById(R.id.velocityValue);
         velocity = findViewById(R.id.velocity);
         up = "go";
@@ -84,5 +88,25 @@ public class Steering extends AppCompatActivity {
         String theValue = ""+value;
         bluetoothConnection.write(theValue.getBytes());
         speed.setText("The speed is set to :" +theValue);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.more:
+                Intent intent2 = new Intent(Steering.this, MainActivity.class);
+                startActivity(intent2);
+                finish();
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                //return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
