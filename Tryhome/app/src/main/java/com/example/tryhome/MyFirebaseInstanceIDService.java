c7193cbfe20ad0b0048814037939527fc0875e15
Bleinc <olivier.bleinc@groupe-esigelec.org>
2020-04-24 14:43:29 +0200

    gradle

--- Tryhome/app/src/main/java/com/example/tryhome/MyFirebaseInstanceIDService.java
+++ Tryhome/app/src/main/java/com/example/tryhome/MyFirebaseInstanceIDService.java
@@ -1,15 +1,16 @@
 package com.example.tryhome;
 
 import android.util.Log;
-import com.google.firebase;
+
 import com.google.firebase.iid.FirebaseInstanceId;
+
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
-public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIDSer {
 
     @Override
     public void OnTokenRefresh() {
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-        Log.d();
+        Log.d("Firebase_refresh", "creating an instance");
     }
 }
